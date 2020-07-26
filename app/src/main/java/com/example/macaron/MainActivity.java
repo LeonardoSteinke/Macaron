c583594433f492b35d3138d665ed1e34288ab55d
Leonardo Tadeu Jaques Steinke <leonardosteinke1@gmail.com>
2020-07-25 21:16:06 -0300

    chore: Telas de Login e Cadastro

--- app/src/main/java/com/example/macaron/MainActivity.java
+++ app/src/main/java/com/example/macaron/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.macaron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp;
+    Button btnSignIn;
+    EditText edtUsername;
+    EditText edtPassword;
+    ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+    private void initComponents() {
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        imgLogo = findViewById(R.id.imgLogo);
+        imgLogo.setImageResource(R.mipmap.logo_foreground);
+
     }
 }
